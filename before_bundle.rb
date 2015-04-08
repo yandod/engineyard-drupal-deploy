8056b7efdd7b605e121d81d8f3aba0c37c67afe7
Tomotsugu Kaneko <snizee@gmail.com>
2015-04-08 20:26:13 +0900

    fix for deprecated @configuration variables

--- before_bundle.rb
+++ before_bundle.rb
@@ -1,8 +1,8 @@
 # link sites directory
-if !Dir.exist?(shared_path + "/sites") then
-  run "cp -R #{release_path}/sites #{shared_path}/sites"
+if !Dir.exist?(config.shared_path + "/sites") then
+  run "cp -R #{config.release_path}/sites #{config.shared_path}/sites"
 end
-run "rm -Rf #{release_path}/sites && ln -s #{shared_path}/sites ./sites"
+run "rm -Rf #{config.release_path}/sites && ln -s #{config.shared_path}/sites ./sites"
 
 # set timezon in php.ini
 #sudo "echo 'date.timezone = Asia/Tokyo' > /etc/php/cgi-php5.4/ext-active/timezone.ini"
